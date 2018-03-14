2a679f22816bf3756d83465cbf887c612984457b
Maxim Shaptala <maxshaptala@gmail.com>
2018-03-14 17:59:12 +0200

    added user entity

---
+++ src/main/java/org/itstep/todolist/domain/User.java
@@ -0,0 +1,23 @@
+package org.itstep.todolist.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class User {
+    @Id 
+    @Column(name = "username")
+    String name;
+
+    @Column(name="password")
+    String password;
+
+    @Column(name="enabled")
+    boolean enabled;
+}
